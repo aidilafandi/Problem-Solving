e1721eb3b755b1a025159ad0534f7a05fe9a5cf2
aidilafandi <aidilafandi83@gmail.com>
2021-10-11 19:16:33 +0700

    chapter_10

---
+++ problem_7/Assets/Problem 6/Problem6.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem6 : SpawnController
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         StartCoroutine(CubeSpawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ problem_7/Assets/Script/SpawnController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    //prefab instantiate
+    public GameObject cube;
+
+    private Rigidbody2D rb;
+
+    public float respawnTime = 2f;
+
+    //next spawn time
+    float nextSpawn = 0f;
+
+    void Start()
+    {
+        
+    }
+
+    private void SpawnCube()
+    {
+        GameObject cb = Instantiate(cube) as GameObject;
+        cb.transform.position = new Vector2 (Random.Range(-1f, 1f), Random.Range(-2f, 2f));
+    }
+
+    public IEnumerator CubeSpawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds (respawnTime);
+             SpawnCube();
+        }
+    }
+}
